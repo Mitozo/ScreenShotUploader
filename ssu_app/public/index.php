a58c7c67ebb94f08828752afc5b313ef9976117f
Tsilavina Randrian <tsilavinandriana@gmail.com>
2024-08-23 11:58:02 +0300

    (build) init symfony 7 project

---
+++ ssu_app/public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+
+// echo "Landing page";
+use App\Kernel;
+
+// var_dump(dirname(__DIR__).'/vendor/autoload_runtime.php');die;
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
