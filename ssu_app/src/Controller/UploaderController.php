38087bf1c6a081e3bd68c3aea9e9a2095156728f
Tsilavina Randrian <tsilavinandriana@gmail.com>
2024-09-10 09:55:58 +0300

    (feat) add logic base of removeFile

--- ssu_app/src/Controller/UploaderController.php
+++ ssu_app/src/Controller/UploaderController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -14,5 +16,18 @@ class UploaderController extends AbstractController
         return $this->render('uploader/index.html.twig', [
             'controller_name' => 'UploaderController',
         ]);
+    }
+
+    #[Route('/uploader/doUpload', name : 'upload.imageClipboard')]
+    public function doUpload(Request $request): Response
+    {
+        dd($request);
+        return new JsonResponse(['clipboardFile' => $request, 'upload_file' => true]);
+    }
+
+    #[Route('/uploader/removeFile', name : 'remove.imageClipboard')]
+    public function removeFile(Request $request): Response
+    {
+        return new JsonResponse(['clipboardFile' => $request]);
     }
 }
